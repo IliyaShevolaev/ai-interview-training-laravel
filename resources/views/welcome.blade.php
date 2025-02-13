4fa9dac6b16bdcf4ec26ccd16e223e422385b6bd
iliyashev <iliya.shevolaev@gmail.com>
2025-02-13 18:50:12 +0300

    bootstrap add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,12 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
 <body>
-    test
-
-    <button onclick="startRecognition()">Говорите</button>
+    <button type="button" class="btn btn-primary" onclick="startRecognition()">Говорите</button>
     <p id="result"></p>
 </body>
 
